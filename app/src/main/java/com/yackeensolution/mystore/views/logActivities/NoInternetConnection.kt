ccdf767b284a52a2655b421679ece072ad9c5974
Mohamed Emish <emish52020@gmail.com>
2019-05-13 18:09:38 +0200

    Initial

--- app/src/main/java/com/yackeensolution/mystore/views/logActivities/NoInternetConnection.kt
+++ app/src/main/java/com/yackeensolution/mystore/views/logActivities/NoInternetConnection.kt
@@ -1,6 +1,8 @@
 package com.yackeensolution.mystore.views.logActivities
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.yackeensolution.mystore.R
 
@@ -9,5 +11,12 @@ class NoInternetConnection : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_no_internet_connection)
+
+        val tryAgain: TextView = findViewById(R.id.try_again)
+        tryAgain.setOnClickListener {
+            val mainIntent = Intent(this@NoInternetConnection, SplashScreen::class.java)
+            startActivity(mainIntent)
+            finish()
+        }
     }
 }
